ee3490d9fbdb83ac4a67d00f5c85970ff696ebd2
andela-tadesanya <adeyemi.adesanya@andela.com>
2019-11-13 17:49:43 +0100

    read the csv file

--- journey_avg.py
+++ journey_avg.py
@@ -1,2 +1,12 @@
+import csv
+
+
+def main():
+    with open('data.csv', 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            pass
+
+
 if __name__=='__main__':
     main()
