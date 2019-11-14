a02551f5769e5981f1bc4296a1235b62599efa1e
andela-tadesanya <adeyemi.adesanya@andela.com>
2019-11-14 13:41:09 +0100

    open and read csv file contents

--- journey_avg.py
+++ journey_avg.py
@@ -1,11 +1,14 @@
-import csv
+from datetime import datetime
+import pandas
+
+REPORT_START_DATE = datetime(2015, 3, 1, 00, 00, 00)
+REPORT_END_DATE = datetime(2015, 3, 31, 23, 59, 59)
 
 
 def main():
-    with open('data.csv', 'r') as csv_file:
-        reader = csv.reader(csv_file)
-        for row in reader:
-            pass
+    data = pandas.read_csv('data.csv',
+                           delimiter=',',
+                           names=['Station ID', 'Bike ID', 'Arrival Datetime', 'Departure Datetime'])
 
 
 if __name__=='__main__':
